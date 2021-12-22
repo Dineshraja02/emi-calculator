a5dc6a7d774a64a4aa2ba146e627c768151963c0
Dinesh Raja <dineshraja0403@gmail.com>
2021-12-22 15:22:59 +0530

    excution

--- src/Emi_page/emi.js
+++ src/Emi_page/emi.js
@@ -11,7 +11,9 @@ const Emi = () => {
            <div className="navbar">
                <h2>EMI CALCULATOR</h2>
            </div>
+           <div className="emiPage_Container">
            <form className="emiPage_Form" onSubmit={handleSubmit}>
+                <h3 className="emiForm_Heading">Loan Details</h3>
                 <input type="number" 
                 name="loanAmount"
                 className="emipage_Inputs"
@@ -19,6 +21,7 @@ const Emi = () => {
                 value={values.loanAmount}
                 onChange={handleChange}
                 />
+                {errors.loanAmount && <p>{errors.loanAmount}</p>}
                 <input type="number" 
                 name="interest"
                 className="emipage_Inputs"
@@ -26,18 +29,40 @@ const Emi = () => {
                 value={values.interest}
                 onChange={handleChange}
                 />
+                {errors.interest && <p>{errors.interest}</p>}
                 <input type="number" 
                 name="month"
                 className="emipage_Inputs"
-                placeholder="Months" 
+                placeholder="Periods in Months" 
                 value={values.month}
                 onChange={handleChange}
                 />
+                {errors.month && <p>{errors.month}</p>}
                 <button 
                 className="emiPage_SubmitButton" 
                 type="submit">Calculate</button>
            </form>
-
+           <div className="emiDetails">
+               <h3 className="emiDetail_Heading">EMI Details</h3>
+               <table className="emi_Table">
+                <tr className="table_Header">
+                <th>Month</th>
+                <th>Principal</th>
+                <th>Interest</th>
+                <th>Balance</th>
+                </tr>
+                {emiDetails.map((emi,index)=>{
+                 return(
+                <tr key={index}>
+                <th>{index+1}</th>
+                <th>{emi.principal}</th>
+                <th>{emi.monthInterestAmount}</th>
+                <th>{emi.balance}</th>
+                </tr>
+                 )})}
+                </table>
+           </div>
+           </div>
        </div>
     )
 }
--- src/Emi_page/validateValues.js
+++ src/Emi_page/validateValues.js
@@ -1,8 +1,19 @@
 export default function validateValues(values){
     let errors={}
-    
-       if(!values.loanAmount){
-        errors.name="Enter the Loan Amount"
+    if(!values.loanAmount){
+        errors.loanAmount="Enter the Loan Amount"
+    }else if(values.loanAmount==0){
+        errors.loanAmount = "Enter Non Decimal Value"
+    }
+
+    if(!values.interest){
+        errors.interest = "Enter the interest value"
+    }
+
+    if(!values.month){
+        errors.month="Enter the period"
+    }else if((values.month)== 0){
+        errors.month = "Enter Non Decimal Value"
     }
 
     return errors;
