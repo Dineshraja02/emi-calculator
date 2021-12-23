32a040ac1f9b81e302a1f233d7022d7402efb32d
Dinesh Raja <dineshraja0403@gmail.com>
2021-12-23 10:10:25 +0530

    into integer

--- src/Emi_page/validateValues.js
+++ src/Emi_page/validateValues.js
@@ -1,4 +1,6 @@
 export default function validateValues(values){
+    values.loanAmount=parseInt(values.loanAmount)
+    values.month=parseInt(values.month)
     let errors={}
     if(!values.loanAmount){
         errors.loanAmount="Enter the Loan Amount"
@@ -6,14 +8,14 @@ export default function validateValues(values){
         errors.loanAmount = "Enter greater than 100"
     }
 
+
     if(!values.interest){
         errors.interest = "Enter the interest value"
     }
-
     if(!values.month){
         errors.month="Enter the period"
-    }else if((values.month)===0){
-        errors.month = "Enter Non Decimal Value"
+    }else if(values.month<=0){
+        errors.month = "Enter valid Month"
     }
 
     return errors;
